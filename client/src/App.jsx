0f67c0c5decb1bf351f6a95e176da88f3ee4fc0b
3MaheshKadam <maheshkadam9298@gmail.com>
2024-06-29 22:41:50 +0530

    header section with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import CreateNote from "./pages/CreateNote.jsx";
+import Home from "./pages/Home.jsx";
 const App = () => {
   return (
-    <div className='text-red-900'>App</div>
-  )
-}
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/create-note" element={<CreateNote/>}/>
 
-export default App
+      </Route>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
+
+  );
+};
+
+export default App;
