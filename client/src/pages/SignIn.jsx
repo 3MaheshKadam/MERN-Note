28d4d0fad5e91c88eb43be6b3a44ca00f4a2c995
3MaheshKadam <maheshkadam9298@gmail.com>
2024-07-01 15:11:39 +0530

    signin ui completed

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const SignIn = () => {
-  return <div>signin</div>;
+  const [formData, setFormData] = useState({});
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className=" mx-auto max-w-lg my-20  border rounded-xl  border-teal-300 p-4 bg-teal-50 shadow-lg">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign-in</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:text-secondary">
+          Sign-In
+        </button>
+      </form>
+      <div className="flex gap-2 text-sm mt-3">
+        <span className="text-gray-500">Have an Account?</span>
+        <Link to="/signup" className="text-sm text-blue-500">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
